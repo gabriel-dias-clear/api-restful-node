6d6dad34cb8df703c00a5780f2b4f68c0c205223
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-08-09 11:13:17 -0300

    rest api crud insert

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-module.exports = (app)=>{
+module.exports = app => {
+ 
+    app.get('/', (req, res) => {
 
-app.get('/', (req,res)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>RESTful API</h1><p>Projeto desenvolvido no Curso Completo de JavaScript!</p>');
 
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).end('<h1> Hello </h1>');
+    });
     
-})
-
-}
+};
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,100 @@
+let NeDB = require('nedb'); //lib simula database
+let db = new NeDB({
+    filename: 'users.db',   // nome do arquivo a ser gerado
+    autoload: true          // auto criação do arquivo
+});
+
 
 module.exports = (app)=>{
 
-app.get('/users', (req,res)=>{
+    let route = app.route('/users')
 
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json({ users: [{
-        name: 'Gabriel',
-        email: 'email@email.com',
-        id: 1
-    }] })
+route.get((req,res)=>{
+
+    db.find({}).sort({name:1}).exec((err, users)=>{
+        if(err){
+
+            app.utils.error.send(err, req, res);
+
+        } else {
+
+            res.status(200).json({users})
+
+        }
+    })
 
 })
+
+    let routeId = app.route('/users/:id')
+
+routeId.get((req, res)=>{
+
+    db.findOne({_id:req.params.id}).exec((err, user)=>{
+
+        if(err){
+
+            app.utils.error.send(err, req, res);
+
+        } else {
+
+            res.status(200).json(user)
+
+        }
+
+    })
+
+})
+
+routeId.put((req, res) => {
+
+    if (!app.utils.validator.user(app, req, res)) return false;
+
+    db.update({ _id: req.params.id }, req.body, err => {
+
+        if (err) {
+            app.utils.error.send(err, req, res);
+        } else {
+            res.status(200).json(Object.assign(req.params, req.body));
+        }
+
+    });
+
+});
+
+
+
+
+
+route.post((req,res)=>{
+
+    db.insert(req.body, (err, user)=>{ //primeiro parâmetro: objeto à ser inserido na db , segundo parametro função que tem parametro erro e parametro do objeto
+        
+        if(err){
+
+            app.utils.error.send(err, req, res);
+
+        } else {
+
+            res.status(200).json({user})
+
+        }
+
+    })
+  
+})
+
+routeId.delete((req, res)=>{
+
+    db.remove({ _id: req.params.id }, {}, err=>{
+
+        if (err) {
+            app.utils.error.send(err, req, res);
+        } else {
+            res.status(200).json(req.params);
+        }
+
+    });
+
+});
 
 };
